e0a7f57752215ca0883db3fb8b93efb952ecea2a
Vanessa Bell <vanessa.bell14@gmail.com>
2017-03-04 13:34:35 -0800

    cleaned up boilerplate and added templates

--- project/app.py
+++ project/app.py
@@ -8,9 +8,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/stance')
+def stance():
+    return render_template('stance.html')
 
 
 if __name__ == '__main__':
